9bda9eaa27efe1a0dd3c5bb6ebf1c223fdcadd58
michaelnv <michaelnv@mail.ru>
2023-10-17 22:45:04 +0500

    Fix make.bat file commands

--- make.bat
+++ make.bat
@@ -2,31 +2,18 @@
 
 SET TARGET=origin/main
 
-IF /I "%1"==".DEFAULT_GOAL " GOTO .DEFAULT_GOAL 
-IF /I "%1"=="help" GOTO help
 IF /I "%1"=="lint" GOTO lint
 IF /I "%1"=="format" GOTO format
 GOTO error
 
-:.DEFAULT_GOAL 
-	CALL make.bat =
-	CALL make.bat help
-	GOTO :EOF
-
-:help
-	@grep -E '^[a-zA-Z_-]+:.*?
-	GOTO :EOF
-
 :lint
-	./git-diff-lint -x "ruff check --exit-zero" -b %TARGET%
-	
+	bash %cd%\git-diff-lint -x "ruff check --exit-zero" -b %TARGET%
 	darker --revision %TARGET% --diff --check
 	GOTO :EOF
 
 :format
 	darker --revision %TARGET%
-	
-	./git-diff-lint -x "ruff --fix --silent --exit-zero" -b %TARGET%
+	bash %cd%\git-diff-lint -x "ruff --fix --silent --exit-zero" -b %TARGET%
 	GOTO :EOF
 
 :error
